da3d515e8a6b6c9178f9f68b64ae58182ae3cca8
Kshitij <kshitijg@cse.iitb.ac.in>
2016-11-01 00:27:03 +0530

    attempts at caldroid?

--- lab11/Feeder07/app/src/main/java/com/example/bhramar/feeder07/MainActivity.java
+++ lab11/Feeder07/app/src/main/java/com/example/bhramar/feeder07/MainActivity.java
@@ -15,14 +15,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    CaldroidFragment caldroidFragment = new CaldroidFragment();
-    Bundle args = new Bundle();
-    Calendar cal = Calendar.getInstance();
-    args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
-    args.putInt(CaldroidFragment.YEAR, cal.get(Calendar.YEAR));
-    caldroidFragment.setArguments(args);
-
-    FragmentTransaction t = getSupportFragmentManager().beginTransaction();
-    t.replace(R.id.calendar1, caldroidFragment);
-    t.commit();
+//    CaldroidFragment caldroidFragment = new CaldroidFragment();
+//    Bundle args = new Bundle();
+//    Calendar cal = Calendar.getInstance();
+//    args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
+//    args.putInt(CaldroidFragment.YEAR, cal.get(Calendar.YEAR));
+//    caldroidFragment.setArguments(args);
+//
+//    FragmentTransaction t = getSupportFragmentManager().beginTransaction();
+//    t.replace(R.id.calendar1, caldroidFragment);
+//    t.commit();
 }
