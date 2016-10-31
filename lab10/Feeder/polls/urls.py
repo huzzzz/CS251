ff991acb30842a5d100613f159500411131f9dc3
CHASMAI HUZEFA EBRAHIM <chasmai@cse.iitb.ac.in>
2016-11-01 00:41:35 +0530

    lab10 initial

---
+++ lab10/Feeder/polls/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url
+
+from polls.views import register, user_login
+
+# urlpatterns = patterns[
+#     # url(r'^about/$', views.about, name='about'),
+#     # url(r'^category/(?P<category_name_url>\w+)$', views.category, name='category'),
+#     # url(r'^add_category/$', views.add_category, name='add_category'),
+#     # url(r'^category/(?P<category_name_url>\w+)/add_page/$', views.add_page, name='add_page'),
+#      # ADD NEW PATTERN!
+# ]
+
+urlpatterns = [
+	url(r'^register/$', register, name='register'),
+	url(r'^login/$', user_login, name='login'),
+]	
