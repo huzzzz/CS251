97825638af7f6789c4eeb47128b5533c942f5cf6
BhramarC <bhramar115@gmail.com>
2016-10-31 20:10:03 +0530

    Uploading lab11

---
+++ lab11/Feeder07/app/src/main/java/com/example/bhramar/feeder07/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.bhramar.feeder07;
+
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.roomorama.caldroid.CaldroidFragment;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    CaldroidFragment caldroidFragment = new CaldroidFragment();
+    Bundle args = new Bundle();
+    Calendar cal = Calendar.getInstance();
+    args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
+    args.putInt(CaldroidFragment.YEAR, cal.get(Calendar.YEAR));
+    caldroidFragment.setArguments(args);
+
+    FragmentTransaction t = getSupportFragmentManager().beginTransaction();
+    t.replace(R.id.calendar1, caldroidFragment);
+    t.commit();
+}
