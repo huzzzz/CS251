1496523f671539bd35340c6a77da0fa534e41009
BhramarC <kshitijg@cse.iitb.ac.in>
2016-11-01 14:40:13 +0530

    Added logout button and Calendar to MainActivity, Android

--- lab11/Feeder07/app/src/main/java/com/example/bhramar/feeder07/MainActivity.java
+++ lab11/Feeder07/app/src/main/java/com/example/bhramar/feeder07/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.bhramar.feeder07;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.roomorama.caldroid.CaldroidFragment;
 
@@ -14,15 +17,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    CaldroidFragment caldroidFragment = new CaldroidFragment();
-    Bundle args = new Bundle();
-    Calendar cal = Calendar.getInstance();
-    args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
-    args.putInt(CaldroidFragment.YEAR, cal.get(Calendar.YEAR));
-    caldroidFragment.setArguments(args);
 
-    FragmentTransaction t = getSupportFragmentManager().beginTransaction();
-    t.replace(R.id.calendar1, caldroidFragment);
-    t.commit();
+        CaldroidFragment caldroidFragment = new CaldroidFragment();
+        Bundle args = new Bundle();
+        Calendar cal = Calendar.getInstance();
+        args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
+        args.putInt(CaldroidFragment.YEAR, cal.get(Calendar.YEAR));
+        caldroidFragment.setArguments(args);
+
+        FragmentTransaction t = getSupportFragmentManager().beginTransaction();
+        t.replace(R.id.calendar1, caldroidFragment);
+        t.commit();
+
+    }
+    public void logoutClick(View view){
+        if (view.getId() == R.id.logoutButton){
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
+    }
+
 }
